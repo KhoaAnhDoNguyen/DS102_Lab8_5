f3336652feb7e1dea60d56b15481e585173d4f67
KhoaAnhDoNguyen <117917211+KhoaAnhDoNguyen@users.noreply.github.com>
2023-12-08 10:51:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ from PIL import Image
 import pickle as pkl
 import numpy as np
 
-st.title('Mnist Detection')
+st.title('Handwritten Digit Recognition')
+
 input = open('lrc_mnist.pkl', 'rb')
 model = pkl.load(input)
 
-st.header('Upload an Image')
-image = st.file_uploader('Choose an image', type=(['png', 'jpg', 'jpeg']) )
+st.header('Upload handwritten digit image')
+uploaded_file = st.file_uploader("Choose an image file", type=(['png', 'jpg', 'jpeg']))
 
-if image is not None:
-  image = Image.open(image)
-  st.image(image, caption = 'Test image')
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Test image')
 
-  if st.button('Predict'):
-    image = image.resize((8*8, 1))
-    vector = np.array(image)
-    label = str((model.predict(vector))[0])
+    if st.button('Predict'):
+        image = image.resize((8*8, 1))
+        feature_vector = np.array(image)
+        label = str((model.predict(feature_vector))[0])
 
-    st.header('Result')
-    st.text(label)
+        st.header('Result')
+        st.text(label)
